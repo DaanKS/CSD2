7ef9ce323d2463f6c3c73c079111f69df98e59a7
Daan Schrier <daan1995@live.co.uk>
2021-09-08 21:40:36 +0200

    updated ritmischeplayback.py

--- python_basics/ritmischeplayback.py
+++ python_basics/ritmischeplayback.py
@@ -1,10 +1,16 @@
 import simpleaudio as sa
 from time import sleep
 
+wave_obj = sa.WaveObject.from_wave_file("/home/daanaanaaan/Documents/Samples/Kick_bip.wav")
 beatsPerMin = int(input("Enter # for BPM "))
 quarterNote = 60 / beatsPerMin
 
 numTrigger = int(input("Enter # for Amount of Plays "))
+triggers = []
 
 for i in range(numTrigger):
-    
+    triggers.append(quarterNote * float(input("Note Length float ")))
+
+for i in triggers:
+    play_obj = wave_obj.play()
+    sleep(i)
