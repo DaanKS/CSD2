98afdb48982eac9ab722f471fa68656f559a1e5c
Daan Schrier <daan1995@live.co.uk>
2021-10-11 22:08:19 +0200

    altered groove, added new rhythmic line + sorting by timeStamp

--- CSD2a/eventbased/EBSequencer.py
+++ CSD2a/eventbased/EBSequencer.py
@@ -20,15 +20,23 @@ def make_event(timestamp, instrument):
     return {'timeStamp': timestamp, 'instrument' : instrument}
 events = []
 
-theGroove=[0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25]
+#TODO make this different
+theGroove=[0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5]
 while len(theGroove) > 0:
     timeStamps.append(theGroove[0] * (quarterNote/4))
     theGroove.pop(0)
-
+#remove extraneous timestamp
 timeStamps.pop()
-count = 0
-for i in range(len(timeStamps)):
-    events.append(make_event(timeStamps[count], instruments[random.randint(0, 2)]))
-    count += 1
 
-print (events)
+for i in range(len(timeStamps)):
+    events.append(make_event(timeStamps[count], instruments[random.randint(0, 1)]))
+    count += 1
+#fill in 16th notes
+for sixteenthNote in range(16):
+    events.append(make_event(sixteenthNote, instruments[2]))
+
+#Took this line from Wouter Ensink
+events.sort(key=lambda x: x['timeStamp'])
+
+
+time_zero = time.time()
