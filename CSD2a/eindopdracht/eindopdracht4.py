28345bd7fd4ab8eb5bafa1299dbb7661309ad4a2
Daan Schrier <daan1995@live.co.uk>
2021-10-26 02:41:03 +0200

    eindopdracht4.py is now storage for discarded code

--- CSD2a/eindopdracht/eindopdracht4.py
+++ CSD2a/eindopdracht/eindopdracht4.py
@@ -205,3 +205,36 @@ while True:
             midiFile.writeFile(outf)
 
         exit()
+
+
+     # check if events is not empty
+    if not events:
+        # list contains no items
+        exit()
+
+    # store the current time
+    time_zero = ti.time()
+    print("time zero:", time_zero)
+    # start at index 0 and retrieve reference to current timestamp
+    index = 0
+    event = events[index]
+    ts = event["timestamp"]
+
+    # iterate through time sequence and play sample
+    while True:
+        now = ti.time() - time_zero
+        # did we arrive at the new timestamp?
+        if(now > ts):
+            # play sample and fetch new event and timestamp
+            playEvent(event)
+            print("index ", index )
+            if index >= len(events):
+                event = events[index]
+                ts = event["timestamp"]
+
+            else:
+                # NOTE: or, if you want to loop, you could repeat at index 0
+                # and then add an time-offset for the current bar
+                break
+
+        ti.sleep(0.001)
