87cfa3687b18a914ff0221bca61e9d00b9e568b4
Daan Schrier <daan1995@live.co.uk>
2021-12-06 03:05:24 +0100

    fixed main.cpp with strings

--- CSD2b/inheritance/main.cpp
+++ CSD2b/inheritance/main.cpp
@@ -1,20 +1,25 @@
 #include "keys.h"
 #include "blaas.h"
+#include "string.h"
 
 int main(){
 
-  Keys newKey;
-  Blaas newBlaas;
+  // Keys newKey;
+  // Blaas newBlaas;
+  //
+  // newBlaas.makeSound();
+  // newKey.makeSound();
+  //
+  // newBlaas.setHornLength(6.34);
+  // newKey.setPolyphony(5);
+  // newKey.setPolyphony(6);
 
-  newBlaas.makeSound();
-  newKey.makeSound();
+  Strings newStr("E Open", 17);
 
-  newBlaas.setHornLength(6.34);
-  newKey.setPolyphony(5);
-  newKey.setPolyphony(6);
-
-
-
+  newStr.makeSound();
+  std::cout << newStr.getNumStrings() << " is the amount of strings\n";
+  newStr.setTuning("Double Drop A");
+  newStr.makeSound();
 
 
 
