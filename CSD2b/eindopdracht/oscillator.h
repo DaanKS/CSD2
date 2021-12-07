d9c4f00acdbd2483133dfd766a2eaba82524abb1
Daan Schrier <daan1995@live.co.uk>
2021-12-07 17:45:05 +0100

    added methods to oscillator class

--- CSD2b/eindopdracht/oscillator.h
+++ CSD2b/eindopdracht/oscillator.h
@@ -8,8 +8,16 @@ public:
   double tick();
   virtual double calculate() = 0;
 
+  void setAmplitude(double amplitude);
+  double getAmplitude();
+
+  void setFrequency(double frequency);
+  double getFrequency();
+
+
 protected:
   double samplerate;
   double frequency;
   double phase;
+  double amplitude;
 };
