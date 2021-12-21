5ebf2eeaf5a870ccda5e016eb49232a1b3fdbf39
Daan Schrier <daan1995@live.co.uk>
2021-12-21 10:11:39 +0100

    removed jack from main.cpp

--- CSD2b/supersimpleSynth/main.cpp
+++ CSD2b/supersimpleSynth/main.cpp
@@ -4,10 +4,10 @@
 
 int main(int argc, char **argv){
   //Create JackModule
-  JackModule jack;
+/*  JackModule jack;
   //initiate jack
   jack.init(argv[0]);
-
+*/
   //start simplesynth
   Supersynth synth(44100);//jack.getSamplerate());
   synth.setPitch(36);
@@ -17,7 +17,7 @@ int main(int argc, char **argv){
 
 
   //make JackModule::onProces
- jack.onProcess = [&synth](jack_default_audio_sample_t *inBuf,
+/*  jack.onProcess = [&synth](jack_default_audio_sample_t *inBuf,
      jack_default_audio_sample_t *outBuf, jack_nframes_t nframes) {
 
     for(unsigned int i = 0; i < nframes; i++){
@@ -28,7 +28,7 @@ int main(int argc, char **argv){
   };
 
   jack.autoConnect();
-
+*/
   std::cout << "\n\nPress 'q' ENTER when you want to quit the program.\n";
   bool running = true;
   while (running)
@@ -37,7 +37,7 @@ int main(int argc, char **argv){
     {
       case 'q':
         running = false;
-        jack.end();
+    //    jack.end();
         break;
     }
   }
