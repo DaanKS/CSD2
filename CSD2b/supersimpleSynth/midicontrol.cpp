ff9a31f7e540357f0050b0a30fcf0e99eb6521da
Daan Schrier <daan1995@live.co.uk>
2021-12-22 09:09:33 +0100

    receiving Midi Notes

--- CSD2b/supersimpleSynth/midicontrol.cpp
+++ CSD2b/supersimpleSynth/midicontrol.cpp
@@ -9,36 +9,17 @@ Midicontrol::Midicontrol() : runningMidi(true){
 Midicontrol::~Midicontrol(){}
 
 void Midicontrol::startMidiListening(){
+  std::cout << "Midicontrol = " << std::endl;
   while(true){
+    //std::cout << "While loop" << std::endl;
     event_read = midi_io.read_event(event);
     if(event_read){
+
       //midi_io.write_event(&event); // copy to output
-      cmd=(int)Pm_MessageStatus(event.message)&0xf0;
-      std::cout << "Message Received : " << cmd << std::endl;
-  /*    channel=Pm_MessageStatus(event.message)&0xf;
-      data1=Pm_MessageData1(event.message);
-      std::cout << (int) cmd << " " << (int) channel << " " << (int)
-              data1 << " " << (int) data1 << endl;
-      // only store note_on and note_off
-      if(recording && (cmd == 0x90 || cmd == 0x80)) eventlist.push_back(event);
-      if(cmd & 0x80) {
-        std::cout << "Now recording\n";
-	recording=true;
-        midi_io.reset_timebase();
-      }
-      if(cmd == 0xb0 && data1 == 0x76) {
-        std::cout << "Now recording\n";
-	recording=true;
-        midi_io.reset_timebase();
-      }
-      if(cmd == 0xb0 && data1 == 0x75) break;
-      if(cmd == 0xb0 && data1 == 0x74) break;
-      if(cmd == 0xb0 && data1 == 0x71){
-        looping=true;
-        break;
-      } */
+      cmd=Pm_MessageData1(event.message);
+      std::cout << "Message Received : " << (int)cmd << std::endl;
     }
-    else usleep(10000);    
+    else usleep(10000);
 
   }
 }
