10451f9a4a930cfe03fbd252d0fe4d98fb4ac599
Daan Schrier <daan1995@live.co.uk>
2021-12-23 11:18:00 +0100

    removed eindopdracht

--- CSD2b/eindopdracht/oscillator.cpp
+++
@@ -1,9 +0,0 @@
-#include "oscillator.h"
-
-Oscillator::Oscillator(double samplerate, double phase){}
-Oscillator::~Oscillator();
-
-double Oscillator::tick(){
-  phase += freq / samplerate;
-  return calculate();
-}
