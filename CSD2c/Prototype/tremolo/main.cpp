3612543c454aed1436e6c92a63da67a8feb47625
Daan Schrier <daan1995@live.co.uk>
2022-02-10 16:03:38 +0100

    added parameter changing functionality, rate and amplitude

--- CSD2c/Prototype/tremolo/main.cpp
+++ CSD2c/Prototype/tremolo/main.cpp
@@ -8,12 +8,12 @@ int main(int argc, char **argv){
   jack.init(argv[0]);
 
   //start Tremolo
-  Tremolo trem(jack.getSamplerate);
+  Tremolo trem(jack.getSamplerate());
 
   //make JackModule::onProcess
   jack.onProcess = [&trem](jack_default_audio_sample_t *inBuf,
   jack_default_audio_sample_t *outBuf, jack_nframes_t nframes){
-    for(unsigned int sample = 0; i < nframes; i++){
+    for(unsigned int sample = 0; sample < nframes; sample++){
       outBuf[sample] = trem.outputSample(inBuf[sample]);
     }
     return 0;
@@ -29,7 +29,20 @@ int main(int argc, char **argv){
         running = false;
         jack.end();
         break;
+      case 'w':
+        std::cout << "enter new rate: ";
+        double tempRate;
+        std::cin >> tempRate;
+        trem.setRate(tempRate);
+        break;
+      case 'e':
+        std::cout << "enter new amplitude: ";
+        double tempAmp;
+        std::cin >> tempAmp;
+        trem.setAmplitude(tempAmp);
+        break;
     }
+
   }
   return 0;
 }
