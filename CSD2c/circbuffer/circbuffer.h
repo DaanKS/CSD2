ab85363db8b83055d5f9f24baf589f73ec9941e9
Daan Schrier <daan1995@live.co.uk>
2022-02-12 22:58:18 +0100

    redoing circbuffer.h

--- CSD2c/circbuffer/circbuffer.h
+++ CSD2c/circbuffer/circbuffer.h
@@ -1,23 +1,29 @@
-#include <iostream>
+//This code is intellectual property of Ciska Vriezenga.
+//I altered some of the naamgeving for my own understanding.
 
+#include <iostream>
 #pragma once
+
+typedef unsigned int uint;
 
 class CircBuffer{
 public:
-  CircBuffer(int size, int numSamplesDelay);
+  CircBuffer(); //we use an empty construtor so we can reserve the object before knowing the samplerate
+  CircBuffer(uint size);
   ~CircBuffer();
 
-  void initialize();
+  void initialize(uint size);
+  void setDelayTimeSamps(uint delayTimeSamps);
 
-  void writeToBuffer(double inputSample);
-  double readFromBuffer();
+  inline void writeToBuffer(float inputSample){buffer[writeIndex] = inputSample;}
+  inline double readFromBuffer(){return buffer[readIndex];}
 
+  inline void incrementHeaders(){
+    incrementWriteIndex();
+    incrementReadIndex();
+  }
 
-  int distanceReadWrite();
-
-  void incrementPointers();
-
-protected:
+private:
   int wrapHeader(int head);
 
   int readIndex = 0;
