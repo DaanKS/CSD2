a0a9adc86c175e982918bddad5531b90f480a1be
Daan Schrier <daan1995@live.co.uk>
2022-02-17 01:43:18 +0100

    something I forget

--- CSD2c/Prototype/multieffect/audioeffect.cpp
+++ CSD2c/Prototype/multieffect/audioeffect.cpp
@@ -4,6 +4,7 @@ AudioEffect::AudioEffect(double samplerate) : samplerate(samplerate){
   std::cout << "Audio Effect Constructor" << std::endl;
 }
 AudioEffect::~AudioEffect(){}
+
 void AudioEffect::setDrywet(float drywet){
   this->drywet = drywet;
 }
