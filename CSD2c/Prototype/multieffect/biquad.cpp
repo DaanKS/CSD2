788eef9272e795e194b02cdca64d5097bd2a1bbc
Daan Schrier <daan1995@live.co.uk>
2022-02-19 01:59:18 +0100

    slowly creating a biquad LPF

--- CSD2c/Prototype/multieffect/biquad.cpp
+++ CSD2c/Prototype/multieffect/biquad.cpp
@@ -4,11 +4,11 @@ Biquad::Biquad(double samplerate): AudioEffect(samplerate){}
 Biquad::~Biquad(){}
 
 
-void Biquad::calculateTheta(float cutoffFreq){
-  this->theta = 2 * M_PI * (cutoffFreq / samplerate);
+void Biquad::calculateOmega(float cutoffFreq){
+  this->omega = 2 * M_PI * (cutoffFreq / samplerate);
 }
-void Biquad::calculateAlpha(float theta){
-  this->alpha = (sin(theta) / (2 * qFactor));
+void Biquad::calculateAlpha(float omega){
+  this->alpha = (sin(omega) / (2 * qFactor));
 }
 
 void Biquad::calculateCoefficients(){
--- CSD2c/Prototype/multieffect/biquad.h
+++ CSD2c/Prototype/multieffect/biquad.h
@@ -14,21 +14,21 @@ public:
   void setCutoffFreq(float cutoffFreq);
   void setQFactor(float qFactor);
 
-  float calculateTheta(float cutoffFreq);
-  float calculateAlpha(float theta);
+  float calculatOmega(float cutoffFreq);
+  float calculateAlpha(float omega);
   float calculateCoefficients();
 
 private:
-  inline void calculateBzero(){Bzero = ((1.0 - cos(theta)) / 2.0); }
-  inline void calculateBone(){Bone = (1.0 - cos(theta));}
-  inline void calculateBtwo(){Btwo = ((1.0 - cos(theta)) / 2.0);}
+  inline void calculateBzero(){Bzero = ((1.0 - cos(omega)) / 2.0); }
+  inline void calculateBone(){Bone = (1.0 - cos(omega));}
+  inline void calculateBtwo(){Btwo = ((1.0 - cos(omega)) / 2.0);}
   inline void calculateAzero(){Azero = 1.0 + alpha;}
-  inline void calculateAone(){Aone = -2.0 * cos(theta);}
+  inline void calculateAone(){Aone = -2.0 * cos(omega);}
   inline void calculateAtwo(){Atwo = 1.0 - alpha;}
 
   float cutoffFreq;
   float qFactor;
-  float theta, alpha;
+  float omega, alpha;
 
   float Bzero, Bone, Btwo;
   float Azero, Aone, Atwo;
