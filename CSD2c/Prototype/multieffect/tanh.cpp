8a2a918c5fcac0efe60b1e6e56413d5526d7ba2b
Daan Schrier <daan1995@live.co.uk>
2022-02-21 22:20:50 +0100

    added unique mix object to tanh

--- CSD2c/Prototype/multieffect/tanh.cpp
+++ CSD2c/Prototype/multieffect/tanh.cpp
@@ -5,12 +5,12 @@
 #include "tanh.h"
 
 Tanh::Tanh(double samplerate) : AudioEffect(samplerate){
-
+    mix = std::make_unique<Mix>();
 }
 Tanh::~Tanh(){}
 
 float Tanh::output(float inputSample) {
-    return tanh(inputSample * drive);
+    return (tanh(inputSample * drive) * mix->getB(drywet)) + (inputSample * mix->getA(drywet));
 }
 void Tanh::setDrive(float drive) {
     this->drive = drive;
