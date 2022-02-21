61779d28ec9339ff703ea56fd85eb78e782d02fb
Daan Schrier <daan1995@live.co.uk>
2022-02-21 20:24:04 +0100

    adding drywet fucntionality with square law panning

--- CSD2c/Prototype/multieffect/squarelaw.cpp
+++ CSD2c/Prototype/multieffect/squarelaw.cpp
@@ -3,3 +3,12 @@
 //
 
 #include "squarelaw.h"
+Mix::Mix(){}
+Mix::~Mix(){}
+
+float Mix::getA(float drywet) {
+    return 1 - 0.25 * pow((drywet + 1), 2);
+}
+float Mix::getB(float drywet) {
+    return 1 - pow(((0.5 * drywet) - 0.5), 2);
+}
