ce7a5aee3a97043b110c9af7f13f644ec6e14b52
Daan Schrier <daan1995@live.co.uk>
2022-02-21 20:16:16 +0100

    added squarelaw and changed biquad diff equation

--- CSD2c/Prototype/multieffect/biquad.cpp
+++ CSD2c/Prototype/multieffect/biquad.cpp
@@ -1,89 +1,89 @@
+//
+// Created by Dean on 21/02/2022.
+//
+
 #include "biquad.h"
 
-Biquad::Biquad(double samplerate): AudioEffect(samplerate){
-  his1 = new CircBuffer(3);
-  his1->setDelayTimeSamps(1);
-  his2 = new CircBuffer(3);
-  his2->setDelayTimeSamps(1);
-  his3 = new CircBuffer(3);
-  his3->setDelayTimeSamps(1);
-  his4 = new CircBuffer(3);
-  his4->setDelayTimeSamps(1);
-  his5 = new CircBuffer(3);
-  his5->setDelayTimeSamps(1);
-  his6 = new CircBuffer(3);
-  his6->setDelayTimeSamps(1);
+
+Biquad::Biquad(double samplerate) : AudioEffect(samplerate){
+    this->samplerate = samplerate;
+    his1 = new History;
+    his2 = new History;
+    his3 = new History;
+    his4 = new History;
+    his5 = new History;
+    his6 = new History;
 }
 
 Biquad::~Biquad(){
-  delete his1;
-  his1 = nullptr;
-  delete his2;
-  his2 = nullptr;
-  delete his3;
-  his3 = nullptr;
-  delete his4;
-  his4 = nullptr;
-  delete his5;
-  his5 = nullptr;
-  delete his6;
-  his6 = nullptr;
+    delete his1;
+    his1 = nullptr;
+    delete his2;
+    his2 = nullptr;
+    delete his3;
+    his3 = nullptr;
+    delete his4;
+    his4 = nullptr;
+    delete his5;
+    his5 = nullptr;
+    delete his6;
+    his6 = nullptr;
 }
 
 
 float Biquad::output(float inputSample){
 /*
   outputSample =  ((1.0 / Azero) * ((Bzero * inputSample) +
-  (Bone * his1->tick(inputSample)) +
-  (Btwo * his2->tick(his3->tick(inputSample))) -
-  (Aone * his4->tick(outputSample)) -
-  (Atwo * his5->tick(his6->tick(outputSample))))); */
-/*
+                    (Bone * his1->tick(inputSample)) +
+                    (Btwo * his2->tick(his3->tick(inputSample))) -
+                    (Aone * his4->tick(outputSample)) -
+                    (Atwo * his5->tick(his6->tick(outputSample)))));
+
   outputSample = (Bzero * inputSample) + (Bone * his1->tick(inputSample)) +
-  Btwo * his2->tick(his3->tick(inputSample)) -
-  Aone * his4->tick(outputSample)-
-  Atwo * his5->tick(his6->tick(outputSample)); */
+          Btwo * his2->tick(his3->tick(inputSample)) -
+          Aone * his4->tick(outputSample)-
+          Atwo * his5->tick(his6->tick(outputSample));
 
-  outputSample = ((Bzero / Azero) * inputSample) +
-  ((Bone / Azero) * his1->tick(inputSample)) +
-  ((Btwo / Azero) * his2->tick(his3->tick(inputSample))) -
-  ((Aone / Azero) * his4->tick(outputSample)) -
-  ((Atwo / Azero) * his5->tick(his6->tick(outputSample)));
-  std::cout << outputSample << std::endl;
-  his1->incrementIndeces();
-  his2->incrementIndeces();
-  his3->incrementIndeces();
-  his4->incrementIndeces();
-  his5->incrementIndeces();
-  his6->incrementIndeces();
+    outputSample = ((Bzero / Azero) * inputSample) +
+                   ((Bone / Azero) * his1->tick(inputSample)) +
+                   ((Btwo / Azero) * his2->tick(his3->tick(inputSample))) -
+                   ((Aone / Azero) * his4->tick(outputSample)) -
+                   ((Atwo / Azero) * his5->tick(his6->tick(outputSample))); */
+    // std::cout << outputSample << std::endl;
+    //y[n] = a0 * x[n] + a1 * x[n-1] + a2 * x[n-2] - b1 * y[n-1] - b2 * y[n-2];
 
-  return 0;
+    outputSample = (Azero * inputSample) +
+                   (Aone * his1->tick(inputSample)) +
+                   (Atwo * his2->tick(his3->tick(inputSample))) -
+                   (Bone * his4->tick(outputSample)) -
+                   (Btwo * his5->tick(his6->tick(outputSample)));
+    return outputSample;
 }
 
 void Biquad::setCutoffFreq(float cutoffFreq){
-  this->cutoffFreq = cutoffFreq;
-  std::cout << cutoffFreq << " CutOff" << std::endl;
+    this->cutoffFreq = cutoffFreq;
+    std::cout << cutoffFreq << " CutOff" << std::endl;
 }
 void Biquad::setQFactor(float qFactor){
-  this->qFactor = qFactor;
-  std::cout << qFactor << " qFactor" << std::endl;
+    this->qFactor = qFactor;
+    std::cout << qFactor << " qFactor" << std::endl;
 }
 
 void Biquad::calculateOmega(){
-  this->omega = 2 * M_PI * (cutoffFreq / samplerate);
-  std::cout << omega << " Omega" << std::endl;
-  std::cout << samplerate << " Calculate Omega" << std::endl;
+    this->omega = 2 * M_PI * (cutoffFreq / samplerate);
+    std::cout << omega << " Omega" << std::endl;
+    std::cout << samplerate << " Calculate Omega" << std::endl;
 }
 void Biquad::calculateAlpha(){
-  this->alpha = (sin(omega) / (2 * qFactor));
-  std::cout << alpha << " alpha" <<std::endl;
+    this->alpha = (sin(omega) / (2 * qFactor));
+    std::cout << alpha << " alpha" <<std::endl;
 }
 
 void Biquad::calculateCoefficients(){
-  calculateBzero();
-  calculateBone();
-  calculateBtwo();
-  calculateAzero();
-  calculateAone();
-  calculateAtwo();
+    calculateBzero();
+    calculateBone();
+    calculateBtwo();
+    calculateAzero();
+    calculateAone();
+    calculateAtwo();
 }
--- CSD2c/Prototype/multieffect/biquad.h
+++ CSD2c/Prototype/multieffect/biquad.h
@@ -1,50 +1,56 @@
-#include "circbuffer.h"
+//
+// Created by Dean on 21/02/2022.
+//
+
 #include "audioeffect.h"
 #include "history.h"
 #include <cmath>
+#include <iostream>
 
 #pragma once
 
 class Biquad : public AudioEffect{
 public:
-  Biquad(double samplerate);
-  ~Biquad();
+    Biquad(double samplerate);
+    ~Biquad();
 
-  float output(float inputSample) override;
+    float output(float inputSample);
 
-  void setCutoffFreq(float cutoffFreq);
-  void setQFactor(float qFactor);
+    void setCutoffFreq(float cutoffFreq);
+    void setQFactor(float qFactor);
 
-  void calculateOmega();
-  void calculateAlpha();
-  void calculateCoefficients();
+    void calculateOmega();
+    void calculateAlpha();
+    void calculateCoefficients();
 
 private:
-  inline void calculateBzero(){this->Bzero = ((1.0 - cos(omega)) / 2.0);
-  std::cout << Bzero << " Bzero" << std::endl; }
-  inline void calculateBone(){this->Bone = (1.0 - cos(omega));
-  std::cout << Bone << " Bone" << std::endl; }
-  inline void calculateBtwo(){this->Btwo = ((1.0 - cos(omega)) / 2.0);
-  std::cout << Btwo << " Btwo" << std::endl; }
-  inline void calculateAzero(){this->Azero = 1.0 + alpha;
-  std::cout << Azero << " Azero" << std::endl; }
-  inline void calculateAone(){this->Aone = -2.0 * cos(omega);
-  std::cout << Aone << " Aone" << std::endl; }
-  inline void calculateAtwo(){this->Atwo = 1.0 - alpha;
-  std::cout << Atwo << " Atwo" << std::endl; }
+    inline void calculateBzero(){this->Bzero = ((1.0 - cos(omega)) / 2.0);
+        std::cout << Bzero << " Bzero" << std::endl; }
+    inline void calculateBone(){this->Bone = (1.0 - cos(omega));
+        std::cout << Bone << " Bone" << std::endl; }
+    inline void calculateBtwo(){this->Btwo = ((1.0 - cos(omega)) / 2.0);
+        std::cout << Btwo << " Btwo" << std::endl; }
+    inline void calculateAzero(){this->Azero = 1.0 + alpha;
+        std::cout << Azero << " Azero" << std::endl; }
+    inline void calculateAone(){this->Aone = -2.0 * cos(omega);
+        std::cout << Aone << " Aone" << std::endl; }
+    inline void calculateAtwo(){this->Atwo = 1.0 - alpha;
+        std::cout << Atwo << " Atwo" << std::endl; }
 
-  CircBuffer* his1;
-  CircBuffer* his2;
-  CircBuffer* his3;
-  CircBuffer* his4;
-  CircBuffer* his5;
-  CircBuffer* his6;
+    History* his1;
+    History* his2;
+    History* his3;
+    History* his4;
+    History* his5;
+    History* his6;
 
-  float cutoffFreq;
-  float qFactor;
-  float omega, alpha;
-  float outputSample;
+    double samplerate;
 
-  float Bzero, Bone, Btwo;
-  float Azero, Aone, Atwo;
+    float cutoffFreq;
+    float qFactor;
+    float omega, alpha;
+    float outputSample;
+
+    float Bzero, Bone, Btwo;
+    float Azero, Aone, Atwo;
 };
