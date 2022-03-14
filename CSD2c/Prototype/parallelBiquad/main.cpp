01d78ea00fede9831e3d94aae0b00e8dfca90f70
Daan Schrier <daan1995@live.co.uk>
2022-03-14 22:05:51 +0100

    changed some magic numbers in man

--- CSD2c/Prototype/parallelBiquad/main.cpp
+++ CSD2c/Prototype/parallelBiquad/main.cpp
@@ -24,8 +24,8 @@ struct MyCallback : AudioIODeviceCallback {
             float tempSample_1 = wave_1->output(biquad->output(input[sample * 2]));
             float tempSample_2 = wave_2->output(biqhpf->output(input[sample * 2]));
 
-            output[sample * 2] = (tempSample_1 + tempSample_2) / 2.0;
-            output[sample * 2 + 1] = (tempSample_1 + tempSample_2) / 2.0;
+            output[sample * 2] = tempSample_1;
+            output[sample * 2 + 1] = tempSample_2;
         }
     }
     void releaseResources() override {}
@@ -40,16 +40,16 @@ int main() {
     float samplerate = 44100;
 
     Waveshaper waveshaper;
-        waveshaper.setKvalue(5.0);
+        waveshaper.setKvalue(100.0);
         waveshaper.setSamplerate(samplerate);
     myCallback.wave_1 = &waveshaper;
     Waveshaper waveshaper2;
-        waveshaper2.setKvalue(1.0);
+        waveshaper2.setKvalue(100.0);
         waveshaper2.setSamplerate(samplerate);
     myCallback.wave_2 = &waveshaper2;
     Biquad biquad2;
         biquad2.setSamplerate(samplerate);
-        biquad2.setCutoffFreq(1000);
+        biquad2.setCutoffFreq(500);
         biquad2.setQFactor(1.0);
         biquad2.calculateOmega();
         biquad2.calculateAlpha();
@@ -57,7 +57,7 @@ int main() {
     myCallback.biquad = &biquad2;
     BiqHPF biquad3;
         biquad3.setSamplerate(samplerate);
-        biquad3.setCutoffFreq(1000);
+        biquad3.setCutoffFreq(200);
         biquad3.setQFactor(1.0);
         biquad3.calculateOmega();
         biquad3.calculateAlpha();
@@ -87,10 +87,13 @@ int main() {
             case 'e':
                 std::cout << "Enter New Value for Cutoff: ";
                 std::cin >> tempValue;
+
                 biquad2.setCutoffFreq(tempValue);
                 biquad3.setCutoffFreq(tempValue);
+
                 std::cout << "Enter New Value for Qfactor: ";
                 std::cin >> tempValue;
+
                 biquad2.setQFactor(tempValue);
                 biquad2.calculateOmega();
                 biquad2.calculateAlpha();
