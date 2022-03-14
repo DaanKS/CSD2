bf27a1e2092fc05ff485cb178df0989995bc8650
Daan Schrier <daan1995@live.co.uk>
2022-03-14 12:00:22 +0100

    cleaned main

--- CSD2c/Prototype/parallelBiquad/main.cpp
+++ CSD2c/Prototype/parallelBiquad/main.cpp
@@ -15,34 +15,17 @@
 struct MyCallback : AudioIODeviceCallback {
     AudioEffect* wave;
     AudioEffect* biquad;
-    AudioEffect* mod_L;
-    AudioEffect* mod_R;
-    AudioEffect* trem;
+
 
     void prepareToPlay(int sampleRate, int numSamplesPerBlock) override {
-   /*     wave = new Waveshaper(sampleRate);
-            ((Waveshaper*)wave)->setKvalue(50);
-            ((Waveshaper*)wave)->generateWaveTable();
-        biquad = new Biquad(sampleRate);
-            ((Biquad*)biquad)->setCutoffFreq(500);
-            ((Biquad*)biquad)->setQFactor(0.01);
-            ((Biquad*)biquad)->calculateOmega();
-            ((Biquad*)biquad)->calculateAlpha();
-            ((Biquad*)biquad)->calculateCoefficients();
-        mod_L = new ModDelay(sampleRate);
-            ((ModDelay*)mod_L)->setRate(0.1);
-        mod_R = new ModDelay(sampleRate);
-            ((ModDelay*)mod_R)->setRate(0.13);
-        trem = new Tremolo(sampleRate);
-            ((Tremolo*)trem)->setRate(1.0);
-            ((Tremolo*)trem)->setAmplitude(0.0); */
+
     }
     void process(float* input, float* output, int numSamples, int numChannels) override {
         for(int sample = 0; sample < numSamples; ++ sample){
-            float tempSample = biquad->output(trem->output(wave->output(input[sample * 2])));
+            float tempSample = biquad->output(wave->output(input[sample * 2]));
 
-            output[sample * 2] = mod_L->output(tempSample);
-            output[sample * 2 + 1] = mod_R->output(tempSample);
+            output[sample * 2] = tempSample;
+            output[sample * 2 + 1] = tempSample;
         }
     }
     void releaseResources() override {}
@@ -68,20 +51,6 @@ int main() {
         biquad2.calculateAlpha();
         biquad2.calculateCoefficients();
     myCallback.biquad = &biquad2;
-    ModDelay modDelay_L;
-        modDelay_L.setSamplerate(samplerate);
-        modDelay_L.assignWave();
-        modDelay_L.setRate(0.1);
-    myCallback.mod_L = &modDelay_L;
-    ModDelay modDelay_R;
-        modDelay_R.setSamplerate(samplerate);
-        modDelay_R.assignWave();
-        modDelay_R.setRate(0.23);
-    myCallback.mod_R = &modDelay_R;
-    Tremolo tremo;
-        tremo.setSamplerate(samplerate);
-        tremo.assignWave();
-    myCallback.trem = &tremo;
 
     try {
         portAudio.setup(44100, 512);
@@ -114,18 +83,7 @@ int main() {
                 biquad2.calculateAlpha();
                 biquad2.calculateCoefficients();
                 break;
-            case 'r': //TODO --- add setters for modDepth and Offset and Feedback
-                std::cout << "Enter New Value for Rate L: ";
-                std::cin >> tempValue;
-                modDelay_L.setRate(tempValue);
-                std::cout << "Enter New Value for Rate R: ";
-                std::cin >> tempValue;
-                modDelay_R.setRate(tempValue);
-                break;
-            case 't':
-                std::cout << "Enter New Value for Tremo Rate: ";
-                std::cin >> tempValue;
-                tremo.setRate(tempValue);
+            
 
         }
     }
