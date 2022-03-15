61fe8129ead72c8ffb1399464cf33f891a683bd9
Daan Schrier <daan1995@live.co.uk>
2022-03-15 20:31:08 +0100

    added coefficient formulas

--- CSD2c/Prototype/parallelBiquad/bandpass.cpp
+++ CSD2c/Prototype/parallelBiquad/bandpass.cpp
@@ -11,3 +11,22 @@ void Bandpass::setBandWidth(float bandwidth) {
 void Bandpass::calculateAlpha() {
     this->alpha = sin(omega) * sinh((log(2)/ 2.0) * m_bandwidth * (omega / sin(omega)));
 }
+
+void Bandpass::calculateBzero() {
+     this->Bzero = (sin(omega) / 2);
+}
+void Bandpass::calculateBone() {
+    this->Bone = 0;
+}
+void Bandpass::calculateBtwo() {
+    this->Btwo = -1 * (sin(omega) / 2);
+}
+void Bandpass::calculateAzero() {
+    this->Azero = 1 + alpha;
+}
+void Bandpass::calculateAone() {
+    this->Aone = -2 * cos(omega);
+}
+void Bandpass::calculateAtwo() {
+    this->Atwo = 1 - alpha;
+}
