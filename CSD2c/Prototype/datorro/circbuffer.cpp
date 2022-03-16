3a5d575aa24fd5d516ec7e623d3f8fc5eef0a9b5
Daan Schrier <daan1995@live.co.uk>
2022-03-16 22:09:41 +0100

    added dependancies

---
+++ CSD2c/Prototype/datorro/circbuffer.cpp
@@ -0,0 +1,35 @@
+#include "circbuffer.h"
+
+//dummy constructor, give it nothing  to start off
+CircBuffer::CircBuffer(){}
+
+CircBuffer::CircBuffer(uint size) : m_size(size),writeIndex(0), readIndex(0){
+  allocateBuffer();
+}
+CircBuffer::~CircBuffer(){
+  deleteBuffer();
+}
+
+void CircBuffer::initialize(uint size){
+  m_size = size;
+  deleteBuffer();
+  allocateBuffer();
+}
+void CircBuffer::allocateBuffer(){
+  //std::cout << "buffer allocation " << std::endl;
+  //reserve and clear buffer. Set 0 for entire size of buffer
+  buffer = (float*)malloc(m_size * sizeof(float));
+  memset(buffer, 0, m_size * sizeof(float));
+}
+void CircBuffer::deleteBuffer(){
+  //using memset to release the reserved space
+  free(buffer);
+}
+
+void CircBuffer::setDelayTimeSamps(uint delayTimeSamps){
+  this->delayTimeSamps = delayTimeSamps;
+  //std::cout << "delayTime Samps" << delayTimeSamps << std::endl;
+  //TODO --- understand this
+  readIndex = writeIndex - delayTimeSamps + m_size;
+  wrapHeader(readIndex);
+}
---
+++ CSD2c/Prototype/datorro/circbuffer.h
@@ -0,0 +1,69 @@
+//This code is intellectual property of Ciska Vriezenga.
+//I altered some of the naamgeving for my own understanding.
+
+#include <iostream>
+#pragma once
+
+typedef unsigned int uint;
+
+class CircBuffer{
+public:
+  CircBuffer(); //we use an empty constructor so we can reserve the object before knowing the samplerate
+  CircBuffer(uint size);
+  ~CircBuffer();
+
+  void initialize(uint size);
+  void setDelayTimeSamps(uint delayTimeSamps);
+
+  inline float tick(float inputSample){writeToBuffer(inputSample);return readFromBuffer();}
+  inline void writeToBuffer(float inputSample){buffer[(uint)writeIndex] = inputSample;}
+  inline float readFromBuffer(){
+
+      float temp_High = buffer[(uint)(readIndex + 0.5)];
+      float temp_Low = buffer[(uint)readIndex];
+      float temp_input = (uint)(readIndex + 0.5) - readIndex;
+
+      float outputSample = linearMap(temp_input, temp_Low, temp_High);
+      return outputSample;}
+
+  inline void incrementIndeces(){
+    incrementWriteIndex();
+    incrementReadIndex();
+  }
+
+  inline float mapInRange(float input, float xLow, float xHigh, float yLow, float yHigh){
+      return (yLow * (xHigh - input) + yHigh * (input - xLow)) / (xHigh - xLow);
+  }
+  inline float linearMap(float input, float low, float high){
+      return mapInRange(input, 0, 1, low, high);
+  }
+
+private:
+//Define incrementmethods. We make them private because we don't want anything
+//external to accidently mess with the header positions seperately.
+  inline void incrementWriteIndex(){
+    writeIndex++;
+    wrapHeader(writeIndex);
+  }
+  inline void incrementReadIndex(){
+    readIndex++;
+    wrapHeader(readIndex);
+  }
+//wrapping function. Using pointers, wow this is super smart @ciska.
+//By giving the index as a pointer we are able to handle it as if we are changing
+//the index as if it was an object. "index" is replaced by "readIndex" or "writeIndex"
+  inline void wrapHeader(float& index){
+    if(index >= m_size){
+       index -= m_size;
+    }
+  }
+
+void allocateBuffer();
+void deleteBuffer();
+
+  float* buffer;
+  uint m_size;
+  uint delayTimeSamps;
+    float writeIndex;
+    float readIndex;
+};
