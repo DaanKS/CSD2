39a3aac0401cbe5fedf0aa1eaf970d293ed757a3
Daan Schrier <daan1995@live.co.uk>
2022-03-20 21:42:07 +0100

    maknig main

---
+++ CSD2c/Prototype/datorro/main.cpp
@@ -0,0 +1,104 @@
+#include "port_audio.h"
+#include "datorro.h"
+#include <iostream>
+#include <stdexcept>
+#include <exception>
+#include <vector>
+
+
+
+struct MyCallback : AudioIODeviceCallback {
+    Datorro* datorro;
+
+
+    auto prepareToPlay(int sampleRate, int numSamplesPerBlock) -> void override {
+
+    }
+    auto process(float* input, float* output, int numSamples, int numChannels) -> void override {
+        for(auto sample = 0; sample < numSamples; ++ sample){
+            auto tempSample = datorro->output(input[sample * 2]);
+
+            auto tempSample_1 = datorro->outputL(tempSample + (tempSample_2 * 0.5));
+            auto tempSample_2 = datorro->outputR(tempSample + (tempSample_1 * 0.5));
+
+            output[sample * 2] = tempSample_1;
+            output[sample * 2 + 1] = tempSample_2;
+        }
+    }
+    auto releaseResources() -> void override {}
+
+};
+
+auto main() -> int {
+
+    auto myCallback = MyCallback();
+    auto portAudio = PortAudio(myCallback);
+
+    auto samplerate = 44100.0f;
+
+    auto waveshaper = Waveshaper();
+        waveshaper.setKvalue(100.0);
+        myCallback.wave_1 = &waveshaper;
+    auto waveshaper2 = Waveshaper();
+        waveshaper2.setKvalue(100.0);
+        myCallback.wave_2 = &waveshaper2;
+
+    auto biquad_1 = Biquad();
+        biquad_1.setCoefficients(biquad_1.makeLowPass(500, 3.0, samplerate));
+        myCallback.biquad = &biquad_1;
+    auto biquad_2 = Biquad();
+        biquad_2.setCoefficients(biquad_2.makeHighPass(700, 3.0, samplerate));
+        myCallback.biqhpf = &biquad_2;
+    auto biquad_3 = Biquad();
+        biquad_3.setCoefficients(biquad_3.makeBandPass(600, 3.0, samplerate, 2.0));
+        myCallback.bandpass = &biquad_3;
+
+    try {
+        portAudio.setup(44100, 512);
+    }
+    catch (std::runtime_error& e) {
+        std::cerr << "error: " << e.what() << "\n";
+    }
+
+    /* waiting / control loop */
+    auto  tempValue = 0.0f;
+    auto running = true;
+    while(running) {
+        switch (std::cin.get()) {
+            case 'q':
+                running = false;
+                break;
+            case 'w':
+                std::cout << "Enter New Value for K value: ";
+                std::cin >> tempValue;
+                waveshaper.setKvalue(tempValue);
+                break;
+            case 'e':
+                std::cout << "Enter New Value for Cutoff: ";
+                std::cin >> tempValue;
+                auto tempDistance = 0.0f;
+                std::cout << "Enter New Value for Distance between the two filters: ";
+                std::cin >> tempDistance;
+
+
+                std::cout << "Enter New Value for Qfactor: ";
+                std::cin >> tempValue;
+
+
+                break;
+
+
+        }
+    }
+    //std::cin.get();
+
+    try {
+        portAudio.teardown();
+    }
+    catch (std::runtime_error& e) {
+        std::cerr << "error: " << e.what() << "\n";
+        return -1;
+    }
+
+    return 0;
+}
