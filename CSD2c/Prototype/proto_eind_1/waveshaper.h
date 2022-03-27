51d86d1a34d24679dfcd17728570566c6e70233f
Daan Schrier <daan1995@live.co.uk>
2022-03-27 18:53:35 +0200

    added waveshaper to folder

---
+++ CSD2c/Prototype/proto_eind_1/waveshaper.h
@@ -0,0 +1,31 @@
+#include <cmath>
+#include <iostream>
+#include "squarelaw.h"
+
+class Waveshaper{
+public:
+    Waveshaper(double samplerate);
+    ~Waveshaper();
+
+    void generateWaveTable();
+
+    float output(float inputSample);
+
+    float mapInRange(float input, float xLow, float xHigh, float yLow, float yHigh);
+    float linearMap(float input, float low, float high);
+
+    void setKvalue(float kValue);
+    void setBufferSize(int bufferSize);
+    void setDryWet(float drywet);
+
+    float clipping(float inputSample, float c_threshold);
+    
+protected:
+    std::unique_ptr<Mix> mix;
+    float* buffer;
+
+    int bufferSize;
+    float kValue;
+    float m_drywet;
+    //float wetSample;
+};
