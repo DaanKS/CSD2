bafc1ca06bd873fcd02b6379614930e722c4269c
Daan Schrier <daan1995@live.co.uk>
2022-04-21 21:51:27 +0200

    added a knob

--- CSD2d/synth/MainComponent.cpp
+++ CSD2d/synth/MainComponent.cpp
@@ -3,6 +3,11 @@
 //==============================================================================
 MainComponent::MainComponent()
 {
+
+    addAndMakeVisible(cutoffSlider);
+    cutoffSlider.setSliderStyle(juce::Slider::SliderStyle::RotaryVerticalDrag);
+    cutoffSlider.setTextBoxStyle(juce::Slider::TextBoxLeft, true, 100, 20);
+
     setSize (600, 400);
 }
 
@@ -12,9 +17,9 @@ void MainComponent::paint (juce::Graphics& g)
     // (Our component is opaque, so we must completely fill the background with a solid colour)
     g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
 
-    g.setFont (juce::Font (16.0f));
-    g.setColour (juce::Colours::white);
-    g.drawText ("Hello World!", getLocalBounds(), juce::Justification::centred, true);
+    g.setFont (juce::Font (32.0f));
+    g.setColour (juce::Colours::black);
+    g.drawText ("Mr. Self Destruct!", getLocalBounds(), juce::Justification::centred, true);
 }
 
 void MainComponent::resized()
@@ -22,4 +27,5 @@ void MainComponent::resized()
     // This is called when the MainComponent is resized.
     // If you add any child components, this is where you should
     // update their positions.
+    cutoffSlider.setBounds(getWidth() / 2.0f, getHeight() / 2.0f, 200, 200);
 }
--- CSD2d/synth/MainComponent.h
+++ CSD2d/synth/MainComponent.h
@@ -24,6 +24,7 @@ public:
 private:
     //==============================================================================
     // Your private member variables go here...
+    juce::Slider cutoffSlider;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
 };
