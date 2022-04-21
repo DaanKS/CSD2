242e3edcd29ddbb607e3b3fdac19a6ca09553285
Daan Schrier <daan1995@live.co.uk>
2022-04-21 21:27:39 +0200

    making JUCE GUI standalone

--- CSD2d/synth/main.cpp
+++ CSD2d/synth/main.cpp
@@ -1,49 +1,101 @@
-#include <iostream>
-#include <juce_audio.h>
-#include "subsynth.h"
+#include "MainComponent.h"
 
-struct TestCallback : AudioCallback
+//==============================================================================
+class GuiAppApplication  : public juce::JUCEApplication
 {
-    void process (const float** input, float** output, int numInputChannels, int numOutputChannels, int numSamples) override
+public:
+    //==============================================================================
+    GuiAppApplication() {}
+
+    // We inject these as compile definitions from the CMakeLists.txt
+    // If you've enabled the juce header with `juce_generate_juce_header(<thisTarget>)`
+    // you could `#include <JuceHeader.h>` and use `ProjectInfo::projectName` etc. instead.
+    const juce::String getApplicationName() override       { return JUCE_APPLICATION_NAME_STRING; }
+    const juce::String getApplicationVersion() override    { return JUCE_APPLICATION_VERSION_STRING; }
+    bool moreThanOneInstanceAllowed() override             { return true; }
+
+    //==============================================================================
+    void initialise (const juce::String& commandLine) override
     {
-        for(int channel = 0; channel < numOutputChannels; ++channel) {
-            for (int sample = 0; sample < numSamples; ++sample) {
-                output[channel][sample] = synth[channel]->output();
-                synth[channel]->updatePitches();
-            }
-        }
+        // This method is where you should put your application's initialisation code..
+        juce::ignoreUnused (commandLine);
+
+        mainWindow.reset (new MainWindow (getApplicationName()));
     }
-    Subsynth* synth[2];
+
+    void shutdown() override
+    {
+        // Add your application's shutdown code here..
+
+        mainWindow = nullptr; // (deletes our window)
+    }
+
+    //==============================================================================
+    void systemRequestedQuit() override
+    {
+        // This is called when the app is being asked to quit: you can ignore this
+        // request and let the app carry on running, or call quit() to allow the app to close.
+        quit();
+    }
+
+    void anotherInstanceStarted (const juce::String& commandLine) override
+    {
+        // When another instance of the app is launched while this one is running,
+        // this method is invoked, and the commandLine parameter tells you what
+        // the other instance's command-line arguments were.
+        juce::ignoreUnused (commandLine);
+    }
+
+    //==============================================================================
+    /*
+        This class implements the desktop window that contains an instance of
+        our MainComponent class.
+    */
+    class MainWindow    : public juce::DocumentWindow
+    {
+    public:
+        explicit MainWindow (juce::String name)
+                : DocumentWindow (name,
+                                  juce::Desktop::getInstance().getDefaultLookAndFeel()
+                                          .findColour (ResizableWindow::backgroundColourId),
+                                  DocumentWindow::allButtons)
+        {
+            setUsingNativeTitleBar (true);
+            setContentOwned (new MainComponent(), true);
+
+#if JUCE_IOS || JUCE_ANDROID
+            setFullScreen (true);
+#else
+            setResizable (true, true);
+            centreWithSize (getWidth(), getHeight());
+#endif
+
+            setVisible (true);
+        }
+
+        void closeButtonPressed() override
+        {
+            // This is called when the user tries to close this window. Here, we'll just
+            // ask the app to quit when this happens, but you can change this to do
+            // whatever you need.
+            JUCEApplication::getInstance()->systemRequestedQuit();
+        }
+
+        /* Note: Be careful if you override any DocumentWindow methods - the base
+           class uses a lot of them, so by overriding you might break its functionality.
+           It's best to do all your work in your content component instead, but if
+           you really have to override any DocumentWindow methods, make sure your
+           subclass also calls the superclass's method.
+        */
+
+    private:
+        JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainWindow)
+    };
+
+private:
+    std::unique_ptr<MainWindow> mainWindow;
 };
 
-auto main() -> int{
-
-    TestCallback callback;
-    AudioBackend audioBackend;
-
-    double samplerate = 44100;
-
-    auto subby = Subsynth(samplerate);
-    auto subbx = Subsynth(samplerate);
-
-        callback.synth[0] = &subby;
-        callback.synth[1] = &subbx;
-
-    subby.setPitch(20);
-    subbx.setPitch(20);
-    subby.setDetune(0.2);
-    subbx.setDetune(0.2);
-    subby.setCutoff(500);
-    subbx.setCutoff(500);
-
-    audioBackend.registerCallback (&callback);
-    audioBackend.openDefaultIODevice (2, 2);
-
-
-    std::cin.get();
-
-
-    audioBackend.closeDevice();
-
-    return 0;
-}
+//==============================================================================
+// This macro generates the main() routine that launches the app.
+START_JUCE_APPLICATION (GuiAppApplication)
